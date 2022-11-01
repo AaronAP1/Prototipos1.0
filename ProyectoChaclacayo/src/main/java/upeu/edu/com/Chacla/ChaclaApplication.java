7e82a2759c20e2f54a93e5f554d737634820ce6a
Aaron <91394866+AaronAP1@users.noreply.github.com>
2022-11-01 11:05:04 -0500

    Add files via upload

---
+++ ProyectoChaclacayo/src/main/java/upeu/edu/com/Chacla/ChaclaApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.com.Chacla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaclaApplication.class, args);
+	}
+
+}
---
+++ ProyectoChaclacayo/src/test/java/upeu/edu/com/Chacla/ChaclaApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.com.Chacla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaclaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
